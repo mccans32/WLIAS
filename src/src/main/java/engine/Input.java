349f28bcb71f4ce425f519e4a3c911bde6a70013
mccans32 <sean.mccann32@mail.dcu.ie>
2020-02-02 00:52:39 +0000

    WLIAS-0019 Added intial classes

---
+++ src/src/main/java/engine/Input.java
@@ -0,0 +1,98 @@
+package engine;
+
+import org.lwjgl.glfw.*;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+
+public class Input {
+    private static boolean[] keys = new boolean[GLFW_KEY_LAST];
+    private static boolean[] buttons = new boolean[GLFW_MOUSE_BUTTON_LAST];
+    private static double mouseX, mouseY;
+    private static double scrollX, scrollY;
+
+    private static GLFWKeyCallback keyboard;
+    private static GLFWCursorPosCallback mouseMove;
+    private static GLFWMouseButtonCallback mouseButtons;
+    private static GLFWScrollCallback mouseScroll;
+
+    public Input() {
+        keyboard = new GLFWKeyCallback() {
+            @Override
+            public void invoke(long window, int key, int scancode, int action, int mods) {
+               keys[key] = (action != GLFW_RELEASE);
+
+            }
+        };
+
+        mouseMove = new GLFWCursorPosCallback() {
+            @Override
+            public void invoke(long window, double xpos, double ypos) {
+                mouseX = xpos;
+                mouseY = ypos;
+            }
+        };
+
+        mouseButtons = new GLFWMouseButtonCallback() {
+            @Override
+            public void invoke(long window, int button, int action, int mods) {
+                buttons[button] = (action != GLFW.GLFW_RELEASE);
+            }
+        };
+
+        mouseScroll = new GLFWScrollCallback() {
+            @Override
+            public void invoke(long window, double xoffset, double yoffset) {
+                scrollX += xoffset;
+                scrollY += yoffset;
+            }
+        };
+    }
+
+    public static boolean isKeyDown(int key) {
+        return keys[key];
+    }
+
+    public static boolean isButtonDown(int button) {
+        return buttons[button];
+    }
+
+    public static void destroy() {
+        keyboard.free();
+        mouseMove.free();
+        mouseButtons.free();
+        mouseScroll.free();
+    }
+
+    public static double getMouseX() {
+        return mouseX;
+    }
+
+    public static double getMouseY() {
+        return mouseY;
+    }
+
+    public GLFWScrollCallback getMouseScroll() {
+        return mouseScroll;
+    }
+
+    public static double getScrollX() {
+        return scrollX;
+    }
+
+    public static double getScrollY() {
+        return scrollY;
+    }
+
+    public GLFWCursorPosCallback getMouseMove() {
+        return mouseMove;
+    }
+
+    public GLFWKeyCallback getKeyboard() {
+        return keyboard;
+    }
+
+    public GLFWMouseButtonCallback getMouseButtons() {
+        return mouseButtons;
+    }
+}
---
+++ src/src/main/java/engine/Window.java
@@ -0,0 +1,180 @@
+package engine;
+
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.glfw.GLFWWindowSizeCallback;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+
+import static java.lang.System.currentTimeMillis;
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+
+public class Window {
+    private long window;
+    private int height;
+    private int width;
+    private String title;
+    public int frames;
+    public long time;
+    private float backgroundR, backgroundG, backgroundB, backgroundAlpha;
+    private GLFWWindowSizeCallback windowSizeCallback;
+    private boolean hasResized = false;
+    private boolean fullscreen = false;
+    private int[] windowPosX = new int[1], windowPosY = new int[1];
+
+    //FINALS
+    final boolean ENABLE_VSYNC = true;
+
+    private Input input;
+
+    public static void setErrorCallback(){
+        glfwSetErrorCallback(GLFWErrorCallback.createPrint(System.err));
+    }
+
+    private void setLocalCallbacks() {
+        windowSizeCallback = new GLFWWindowSizeCallback() {
+            @Override
+            public void invoke(long argWindow, int argWidth, int argHeight) {
+                width = argWidth;
+                height = argHeight;
+                hasResized = true;
+            }
+        };
+
+        //Set Size Callback
+        glfwSetWindowSizeCallback(window, windowSizeCallback);
+        //Set input callbacks
+        glfwSetKeyCallback(window, input.getKeyboard());
+        glfwSetCursorPosCallback(window, input.getMouseMove());
+        glfwSetScrollCallback(window, input.getMouseScroll());
+        glfwSetMouseButtonCallback(window, input.getMouseButtons());
+    }
+
+    public Window(int width, int height, String title) {
+        this.width = width;
+        this.height = height;
+        this.title = title;
+    }
+
+    public void create() {
+        setErrorCallback();
+
+        if (!glfwInit()) {
+            System.err.println(("GLFW Failed to start!"));
+            System.exit(1);
+        }
+
+        input = new Input();
+
+        window = glfwCreateWindow(width, height, title, (fullscreen ? glfwGetPrimaryMonitor() : 0), 0);
+
+        if (window == 0) throw new IllegalStateException("Failed to create window!");
+
+        if (!fullscreen) {
+            GLFWVidMode videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+            // X value
+            windowPosX[0] = (videoMode.width() - width) / 2;
+            // Y value
+            windowPosY[0] = (videoMode.height() - height) / 2;
+            glfwSetWindowPos(window, windowPosX[0], windowPosY[0]);
+        }
+
+        //Set Current Context to Window
+        glfwMakeContextCurrent(window);
+        // Enables OpenGL functionality
+        GL.createCapabilities();
+        // Set Depth Test
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        //Set local callbacks
+        setLocalCallbacks();
+        //Display Window
+        showWindow();
+        //Enable VSync
+        setVSync(ENABLE_VSYNC);
+        // set timer for window
+        time = currentTimeMillis();
+
+    }
+
+    public void update() {
+        if (hasResized) {
+            GL11.glViewport(0, 0, width, height);
+            hasResized = false;
+        }
+        GL11.glClearColor(backgroundR, backgroundG, backgroundB, backgroundAlpha);
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+        glfwPollEvents();
+        updateFrameCounter();
+    }
+
+    public void destroy() {
+        Input.destroy();
+        glfwWindowShouldClose(window);
+        glfwDestroyWindow(window);
+    }
+
+    public void swapBuffers() {
+        glfwSwapBuffers(window);
+    }
+
+    public boolean shouldClose() {
+        return glfwWindowShouldClose(window);
+    }
+
+    public void showWindow() {
+        glfwShowWindow(window);
+    }
+
+    public void setVSync(boolean V_sync) {
+        glfwSwapInterval( V_sync ? 1 : 0);
+    }
+
+    public void  setBackgroundColour(float r, float g, float b, float alpha) {
+        backgroundR = r;
+        backgroundG = g;
+        backgroundB = b;
+        backgroundAlpha = alpha;
+    }
+
+    public void updateFrameCounter() {
+        frames++;
+        if (currentTimeMillis() > time + 1000) {
+            glfwSetWindowTitle(window, title + " | FPS " + frames);
+            time = currentTimeMillis();
+            frames = 0;
+        }
+    }
+
+    public boolean isFullscreen() {
+        return fullscreen;
+    }
+
+    public void setFullscreen(boolean fullscreen){
+        this.fullscreen = fullscreen;
+        this.hasResized = true;
+        if (isFullscreen()) {
+            glfwGetWindowPos(window, windowPosX, windowPosY);
+            glfwSetWindowMonitor(window, glfwGetPrimaryMonitor(), 0, 0, width, height, 0);
+        }
+        else {
+            glfwSetWindowMonitor(window, 0, windowPosX[0], windowPosY[0], width, height, 0);
+        }
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isHasResized() {
+        return hasResized;
+    }
+}
