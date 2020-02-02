87b0b378d4c23fb82ce04cb37957cd968c54370c
Sean McCann <sean.mccann32@mail.dcu.ie>
2020-02-02 19:27:16 +0000

    WLIAS-0019
    Added Checkstyle gradle addon, the project now uses the standard Google Java format.

--- src/src/main/java/game/Game.java
+++ src/src/main/java/game/Game.java
@@ -3,61 +3,96 @@ package game;
 import engine.Input;
 import engine.Window;
 import org.lwjgl.glfw.GLFW;
-import org.lwjgl.opengl.GL;
 import java.lang.System;
 
+/**
+ * The type Game.
+ */
 public class Game {
 
-  final int WINDOW_WIDTH = 1200, WINDOW_HEIGHT = 900;
-  final String WINDOW_TILE = "We Live in a Society";
-  final float BACKGROUND_RED = 1.0f;
-  final float BACKGROUND_GREEN = 0.0f;
-  final float BACKGROUND_BLUE = 0.0f;
-  final float BACKGROUND_ALPHA = 1.0f;
-  public Window window;
+    /**
+     * The Window width.
+     */
+    final int WINDOW_WIDTH = 1200, /**
+     * The Window height.
+     */
+    WINDOW_HEIGHT = 900;
+    /**
+     * The Window tile.
+     */
+    final String WINDOW_TILE = "We Live in a Society";
+    /**
+     * The Background red.
+     */
+    final float BACKGROUND_RED = 1.0f;
+    /**
+     * The Background green.
+     */
+    final float BACKGROUND_GREEN = 0.0f;
+    /**
+     * The Background blue.
+     */
+    final float BACKGROUND_BLUE = 0.0f;
+    /**
+     * The Background alpha.
+     */
+    final float BACKGROUND_ALPHA = 1.0f;
+    /**
+     * The Window.
+     */
+    public Window window;
 
-  public void start() {
-    initialize();
-    gameLoop();
-    dispose();
-  }
+    /**
+     * Start.
+     */
+    public void start() {
+        initialize();
+        gameLoop();
+        dispose();
+    }
 
-  private void dispose() {
-    System.out.println("Disposing active processes");
-    window.destroy();
-  }
+    private void dispose() {
+        System.out.println("Disposing active processes");
+        window.destroy();
+    }
 
-  private void gameLoop() {
-    System.out.println("This is the Game Loo\n");
-    // while (true)
-    while (!window.shouldClose() && !Input.isKeyDown(GLFW.GLFW_KEY_ESCAPE)) {
-      update();
-      render();
+    private void gameLoop() {
+        System.out.println("This is the Game Loo\n");
+        // while (true)
+        while (!window.shouldClose() && !Input.isKeyDown(GLFW.GLFW_KEY_ESCAPE)) {
+            update();
+            render();
 
-      //Check for fullscreen
-        if (Input.isKeyDown(GLFW.GLFW_KEY_F11)) {
-            window.setFullscreen(!window.isFullscreen());
+            //Check for fullscreen
+            if (Input.isKeyDown(GLFW.GLFW_KEY_F11)) {
+                window.setFullscreen(!window.isFullscreen());
+            }
         }
     }
-  }
 
-  private void initialize() {
-    System.out.println("Initializing Simulation\n");
-    // Create main window
-    window = new Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TILE);
-    window.setBackgroundColour(BACKGROUND_RED, BACKGROUND_GREEN, BACKGROUND_BLUE, BACKGROUND_ALPHA);
-    window.create();
-  }
-
-  public void update() {
-    window.update();
-    // Temporary
-    if (Input.isButtonDown(GLFW.GLFW_MOUSE_BUTTON_LEFT)) {
-      System.out.println("X: " + Input.getMouseX() + ", Y: " + Input.getMouseY());
+    private void initialize() {
+        System.out.println("Initializing Simulation\n");
+        // Create main window
+        window = new Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TILE);
+        window.setBackgroundColour(BACKGROUND_RED, BACKGROUND_GREEN, BACKGROUND_BLUE, BACKGROUND_ALPHA);
+        window.create();
     }
-  }
 
-  public void render() {
-    window.swapBuffers();
-  }
+    /**
+     * Update.
+     */
+    public void update() {
+        window.update();
+        // Temporary
+        if (Input.isButtonDown(GLFW.GLFW_MOUSE_BUTTON_LEFT)) {
+            System.out.println("X: " + Input.getMouseX() + ", Y: " + Input.getMouseY());
+        }
+    }
+
+    /**
+     * Render.
+     */
+    public void render() {
+        window.swapBuffers();
+    }
 }
