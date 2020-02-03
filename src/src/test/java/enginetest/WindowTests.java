e7b92572e2880455c9a63da23ca3322b3823541c
Sean McCann <sean.mccann32@mail.dcu.ie>
2020-02-03 20:54:23 +0000

    WLIAS--0019
    Adding info tag to check stage

--- src/src/test/java/enginetest/WindowTests.java
+++ src/src/test/java/enginetest/WindowTests.java
@@ -1,14 +1,9 @@
 package enginetest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import engine.Window;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
