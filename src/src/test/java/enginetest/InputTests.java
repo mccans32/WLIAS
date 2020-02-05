fdb5254509092bdbd592ec128b15a7d680b29589
Sean McCann <sean.mccann32@mail.dcu.ie>
2020-02-05 16:06:19 +0000

    WLIAS-0019
    Added unit tests for input class

---
+++ src/src/test/java/enginetest/InputTests.java
@@ -0,0 +1,45 @@
+package enginetest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+
+import engine.Input;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class InputTests {
+  private Input input;
+
+  @BeforeEach
+  void setup() {
+    assumeTrue(glfwInit());
+    input = new Input();
+  }
+
+  @AfterEach
+  public void cleanUpEach() {
+    Input.destroy();
+  }
+
+  @Test
+  public void keyBoardIsSet() {
+    assertNotNull(input.getKeyboard());
+  }
+
+  @Test
+  public void mouseMoveIsSet() {
+    assertNotNull(input.getMouseMove());
+  }
+
+  @Test
+  public void mouseScrollIsSet() {
+    assertNotNull(input.getMouseScroll());
+  }
+
+  @Test
+  public void mouseButtonsIsSet() {
+    assertNotNull(input.getMouseButtons());
+  }
+}
