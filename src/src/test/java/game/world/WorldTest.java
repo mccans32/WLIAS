601944303f398700c420922e787416fb31b76c4f
slowikk2 <kacper.slowikowski2@mail.dcu.ie>
2020-03-18 14:55:12 +0000

    WLIAS-0028_refactoring_mapGeneration_and_testing
    added new test cases to WorldTest class.
    refactored some redundant code in mapGeneration regarding landmasses.

--- src/src/test/java/game/world/WorldTest.java
+++ src/src/test/java/game/world/WorldTest.java
@@ -1,13 +1,19 @@
 package game.world;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import engine.Window;
+import engine.graphics.Vertex3D;
 import engine.objects.world.Camera;
+import engine.objects.world.TileWorldObject;
+import java.util.ArrayList;
+import java.util.List;
 import map.MapGeneration;
+import math.Vector2f;
 import math.Vector3f;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,22 +29,33 @@ public class WorldTest {
   private Camera camera;
   private Window window;
 
+  private static Vector2f calcCentre(TileWorldObject tile) {
+    return new Vector2f(tile.getPosition().getX(), tile.getPosition().getY());
+  }
+
   @BeforeEach
   public void setUp() {
     camera = new Camera(DEFAULT_POSITION, DEFAULT_ROTATION);
     window = new Window(WINDOW_X, WINDOW_Y, TITLE);
-    MapGeneration.setLandMassSizeX(LAND_MASS_SIZES[0]);
-    MapGeneration.setLandMassSizeY(LAND_MASS_SIZES[0]);
-    MapGeneration.setAmountOfAridTiles(LAND_MASS_SIZES[0] * LAND_MASS_SIZES[0]);
-    MapGeneration.setAmountOfWaterTiles(0);
-    MapGeneration.setAmountOfPlainTiles(0);
-    MapGeneration.setAmountOfFertileTiles(0);
-    MapGeneration.createMap();
+    genMap(MapGeneration.getDefaultLandmassSizeX(), MapGeneration.getDefaultLandmassSizeY(),
+        MapGeneration.getDefaultAmountAridTiles(), MapGeneration.getDefaultAmountWaterTiles(),
+        MapGeneration.getDefaultAmountPlainTiles(), MapGeneration.getDefaultAmountFertileTiles());
     window.create();
   }
 
+  private void genMap(int landmassSizeX, int landmassSizeY, int amountAridTiles,
+                      int amountWaterTiles, int amountPlainTiles, int amountFertileTiles) {
+    MapGeneration.setLandMassSizeX(landmassSizeX);
+    MapGeneration.setLandMassSizeY(landmassSizeY);
+    MapGeneration.setAmountOfAridTiles(amountAridTiles);
+    MapGeneration.setAmountOfWaterTiles(amountWaterTiles);
+    MapGeneration.setAmountOfPlainTiles(amountPlainTiles);
+    MapGeneration.setAmountOfFertileTiles(amountFertileTiles);
+    MapGeneration.createMap();
+  }
+
   @Test
-  void create() {
+  void createWorldMap() {
     assertNull(World.getWorldMap());
     World.create(window, camera);
     assertNotNull(World.getWorldMap());
@@ -47,7 +64,23 @@ public class WorldTest {
   @Test
   void cameraBorders() {
     assertEquals(camera.getMaxCameraX(), Camera.getMinCameraBorder());
-    World.create(window, camera);
-    assertNotEquals(camera.getMaxCameraX(), Camera.getMinCameraBorder());
+    for (int size : LAND_MASS_SIZES) {
+      genMap(size, size, (size * size), 0, 0, 0);
+      World.create(window, camera);
+      Vector2f botLeft = calcCentre(World.getWorldMap()[World.getWorldMap().length - 1][0]);
+      Vector2f topRight = calcCentre(World.getWorldMap()[0][World.getWorldMap().length - 1]);
+      assertEquals(camera.getMinCameraX(), botLeft.getX());
+      assertEquals(camera.getMaxCameraX(), topRight.getX());
+      assertEquals(camera.getMaxCameraY(), topRight.getY());
+      assertEquals(camera.getMinCameraY(), botLeft.getY());
+    }
+  }
+
+  @Test
+  void addToVertexListTest() {
+    List<Vertex3D> vertexList = new ArrayList<>();
+    assertTrue(vertexList.isEmpty());
+    World.addToVertexList(vertexList);
+    assertFalse(vertexList.isEmpty());
   }
 }
