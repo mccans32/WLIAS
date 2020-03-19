7e036f5c91bc1f2bfc2fff745b25b46f8cbed900
Sean McCann <sean.mccann32@mail.dcu.ie>
2020-03-19 13:51:11 +0000

    WLIAS-0028 Added extra parameters to Text class
    Added Text.textColour, Text.numColumns, Text.numRows and their respective getters and setters

--- src/src/main/java/engine/graphics/text/Text.java
+++ src/src/main/java/engine/graphics/text/Text.java
@@ -1,8 +1,14 @@
 package engine.graphics.text;
 
+import engine.utils.ColourUtils;
+import java.awt.Color;
+import math.Vector3f;
+
 public class Text {
-  private static final String DEFAULT_FONT_FILE = "/text/defaultFont.png";
-  private String fontFile = DEFAULT_FONT_FILE;
+  private int numColumns = 16;
+  private int numRows = 16;
+  private Vector3f textColour = ColourUtils.convertColor(Color.BLACK);
+  private String fontFile = "/text/defaultFont.png";
   private boolean centreHorizontal = false;
   private boolean centreVertical = false;
   private float fontSize = 1;
@@ -13,14 +19,20 @@ public class Text {
    *
    * @param text             the text
    * @param fontFile         the font file
+   * @param numColumns       the num columns
+   * @param numRows          the num rows
+   * @param textColour       the text colour
    * @param fontSize         the font size
    * @param centreHorizontal the centre horizontal
    * @param centreVertical   the centre vertical
    */
-  public Text(String text, String fontFile, float fontSize, boolean centreHorizontal,
-              boolean centreVertical) {
+  public Text(String text, String fontFile, int numColumns, int numRows, Vector3f textColour,
+              float fontSize, boolean centreHorizontal, boolean centreVertical) {
     this.text = text;
     this.fontFile = fontFile;
+    this.numColumns = numColumns;
+    this.numRows = numRows;
+    this.textColour = textColour;
     this.fontSize = fontSize;
     this.centreHorizontal = centreHorizontal;
     this.centreVertical = centreVertical;
@@ -30,9 +42,17 @@ public class Text {
     this.text = text;
   }
 
-  public Text(String text, float fontSize) {
+  /**
+   * Instantiates a new Text.
+   *
+   * @param text       the text
+   * @param fontSize   the font size
+   * @param textColour the text colour
+   */
+  public Text(String text, float fontSize, Vector3f textColour) {
     this(text);
     this.fontSize = fontSize;
+    this.textColour = textColour;
   }
 
   /**
@@ -43,10 +63,19 @@ public class Text {
    * @param centreHorizontal the centre horizontal
    * @param centreVertical   the centre vertical
    */
-  public Text(String text, float fontSize, boolean centreHorizontal, boolean centreVertical) {
-    this(text, fontSize);
+  public Text(String text, float fontSize, Vector3f textColour, boolean centreHorizontal,
+              boolean centreVertical) {
+    this(text, fontSize, textColour);
     this.centreHorizontal = centreHorizontal;
     this.centreVertical = centreVertical;
+  }
+
+  public int getNumColumns() {
+    return numColumns;
+  }
+
+  public int getNumRows() {
+    return numRows;
   }
 
   public boolean isCentreHorizontal() {
@@ -65,12 +94,16 @@ public class Text {
     this.centreVertical = centreVertical;
   }
 
-  public String getFontFile() {
-    return fontFile;
+  public Vector3f getTextColour() {
+    return textColour;
   }
 
-  public void setFontFile(String fontFile) {
-    this.fontFile = fontFile;
+  public void setTextColour(Vector3f textColour) {
+    this.textColour = textColour;
+  }
+
+  public String getFontFile() {
+    return fontFile;
   }
 
   public float getFontSize() {
