0c49f44bda47ca6504e854aa541fdc8ee3116716
slowikk2 <kacper.slowikowski2@mail.dcu.ie>
2020-03-24 11:45:13 +0000

    WLIAS-0030_Adding_initial_attempt_at_color_overlay
    Changes to shader classes to incorporate the color offset attempt.
    
    Added image for border for a different approach.

--- src/src/main/resources/shaders/vertex/mainVertex.glsl
+++ src/src/main/resources/shaders/vertex/mainVertex.glsl
@@ -6,12 +6,18 @@ in vec2 textureCoords;
 
 out vec3 passColour;
 out vec2 passTextureCoords;
+out vec4 passColourOffset;
+
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+uniform vec3 colourOffset;
+
+float offsetAlpha = 0;
 
 void main() {
     gl_Position = projection * view * model * vec4(position, 1.0);
     passColour = color;
+    passColourOffset = vec4(colourOffset, offsetAlpha);
     passTextureCoords = textureCoords;
 }
