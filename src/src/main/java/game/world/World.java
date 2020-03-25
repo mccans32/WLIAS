c857fdf83492b44785182425b7b210bab6521b67
Sean McCann <sean.mccann32@mail.dcu.ie>
2020-03-25 17:49:37 +0000

    WLIAS-0034 Fixed Tile Claiming Bug
    Fixed a bug within World that was causing the program to crash. The logic for the loop was a while so was infintely running, the logic is now an if check followed by a while.

--- src/src/main/java/game/world/World.java
+++ src/src/main/java/game/world/World.java
@@ -147,16 +147,17 @@ public class World {
       turnCounter = 0;
       for (Society society : societies) {
         boolean claimed = false;
-        while (!claimed && totalClaimedTiles
-            < (MapGeneration.getLandMassSizeX() + 2) * (MapGeneration.getLandMassSizeY() + 2)) {
-          int row = generateRandomRowIndex();
-          int column = generateRandomColumnIndex();
-          if (!worldMap[row][column].isClaimed()) {
-            society.claimTile(worldMap[row][column]);
-            worldMap[row][column].setClaimed(true);
-            bordersAltered = true;
-            totalClaimedTiles++;
-            claimed = true;
+        if (totalClaimedTiles < worldMap.length * worldMap[0].length) {
+          while (!claimed) {
+            int row = generateRandomRowIndex();
+            int column = generateRandomColumnIndex();
+            if (!worldMap[row][column].isClaimed()) {
+              society.claimTile(worldMap[row][column]);
+              worldMap[row][column].setClaimed(true);
+              bordersAltered = true;
+              totalClaimedTiles++;
+              claimed = true;
+            }
           }
         }
       }
@@ -281,12 +282,12 @@ public class World {
    */
   public static int generateRandomRowIndex() {
     Random r = new Random();
-    return r.nextInt(worldMap[0].length - 1) + 1;
+    return r.nextInt(worldMap[0].length);
 
   }
 
   public static int generateRandomColumnIndex() {
     Random r = new Random();
-    return r.nextInt(worldMap[0].length - 1) + 1;
+    return r.nextInt(worldMap[0].length);
   }
 }
