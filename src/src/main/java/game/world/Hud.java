e15fef7d7aea6602c4021e0ae0fef9986c3d00a5
Sean McCann <sean.mccann32@mail.dcu.ie>
2020-04-20 13:34:24 +0100

    WLIAS-0048 Fixed Society String

--- src/src/main/java/game/world/Hud.java
+++ src/src/main/java/game/world/Hud.java
@@ -665,11 +665,11 @@ public class Hud {
     } else {
       societyString = "Society " + (society.getSocietyId() + 1);
     }
-    String startPadding = StringUtils.repeat(" \n", 3);
-    String linePadding = "\n \n";
-    return String.format("%9$s Society Name: %s %10$s Population: %d %10$s Food: %d "
-            + "%10$s Raw Material: %d %10$s Territory Size: %d %10$s Average Aggressiveness: %.2f "
-            + "%10$s Average Productivity: %.2f %10$s Average Age: %.2f %10$s Army Size: %d",
+    String startPadding = StringUtils.repeat(" \n", 5);
+    String linePadding = "\n";
+    return String.format("%10$s Society Name: %s %11$s Population: %d %11$s Food: %d "
+            + "%11$s Raw Material: %d %11$s Territory Size: %d %11$s Average Aggressiveness: %.2f "
+            + "%11$s Average Productivity: %.2f %11$s Average Age: %.2f %11$s Army Size: %d",
         societyString, society.getPopulation().size(), society.getTotalFoodResource(),
         society.getTotalRawMaterialResource(), society.getTerritory().size(),
         society.getAverageAggressiveness(), society.getAverageProductivity(),
